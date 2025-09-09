33c2ae479a2d9cb1784b056b2d50b7e88d0ce971
Alexsandr Lysinskii <70683200+Alexasanr063@users.noreply.github.com>
2025-09-09 18:31:53 +0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,27 @@
+class Data:
+
+    data_for_new_user1 = {
+        "password": "password",
+        "name": "Username"
+    }
+
+    data_for_new_user2 = {
+        "name": "САНЕК"
+    }
+
+    data_for_order =  {
+    "name": "Краторный метеоритный бургер",
+    "order": {
+    "number": 6257
+    },
+    "success": True
+    }
+
+
+    name_1 = "https://stellarburgers.nomoreparties.site/api/auth/register"
+    name_2 = "https://stellarburgers.nomoreparties.site/api/auth/login"
+    name_3 = "https://stellarburgers.nomoreparties.site/api/auth/user"
+    name_4 = "https://stellarburgers.nomoreparties.site/api/orders"
+    name_5 = 'https://stellarburgers.nomoreparties.site/api/orders/all'
+    ingridients = "https://stellarburgers.nomoreparties.site/api/ingredients"
+    ingridients_1 = ['61c0c5a71d1f82001bdaaa6c','61c0c5a71d1f82001bdaaa77']
